b69f516750aa5dd3363525e7efcfef98e5e5b30b
nlarsen7 <nlarsen2013@gmail.com>
2020-04-26 01:48:53 +0000

    3.6.2 Tip

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
+//Nicholas Larsen
+//4/25/2020
+//calculates the tip
 package main
 
 import "fmt"
 func cost(total float64, tip float64)float64{
 return total*tip
+//math used in finding the tip
 }
 func main() {
  total:=754.0
  tip:=0.20
+ //variables set for the total and tip percentage
  fmt.Println(cost(total, tip),"is the amount for your tip")
+ //prints out the tip amount
 }
